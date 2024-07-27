35ea214804b888e6d9724b6b732a547164c4c5c3
Jaylord Lagud <jaylord.lagud.13256@remotestaff.net>
2024-07-28 00:50:56 +0800

    feat: Implement IP address creation service and factory
    - Setup Service Provider for Create Ipaddress service
    - Setup Docker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\IpAddress\Factories\CreateIpAddressFactory;
+use App\Services\IpAddress\Interfaces\CreateIpAddressFactoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+       $this->app->bind(CreateIpAddressFactoryInterface::class, CreateIpAddressFactory::class);
     }
 
     /**
